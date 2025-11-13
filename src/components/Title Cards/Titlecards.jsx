91524cb99ef0f396e0c7538380e0b9b5a0d907c2
yuva78 <yu0011an@e.ntu.edu.sg>
2025-11-13 14:48:44 +0800

    added the nav bar of homepage

---
+++ src/components/Title Cards/Titlecards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Titlecards.css'
+const Titlecards = () => {
+  return (
+    <div className = 'titlecards'>
+      
+    </div>
+  )
+}
+
+export default Titlecards
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import NavBar from '../../components/NavBar/NavBar.jsx'
+
+
+const Home = () => {
+  return (
+    <div className ='home'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default Home
