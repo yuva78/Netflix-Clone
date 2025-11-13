afe30e6daa37cb04feef385d68ee3971970283bd
yuva78 <yu0011an@e.ntu.edu.sg>
2025-11-13 15:08:33 +0800

    adding Wednesday show as a banner

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
-import profile_img from '../../assets/profile_img.png'
+import profile from '../../assets/profile.png'
 import caret_icon from '../../assets/caret_icon.svg'
 
 const NavBar = () => {
@@ -25,7 +25,7 @@ const NavBar = () => {
         <p>Children</p>
         <img src={bell_icon} alt='' className = 'icons' />
         <div className="navbar-profile">
-          <img src={profile_img} alt='' className = 'profile' />
+          <img src={profile} alt='' className = 'profile' />
           <img src={caret_icon} alt=''  />
           <div className="dropdown">
             <p>Sign Out of Netflix</p>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Home.css'
 import NavBar from '../../components/NavBar/NavBar.jsx'
-
+import wed_banner from '../../assets/wed-banner.jpg'
+import wed_title from '../../assets/wed-title.png'
 
 const Home = () => {
   return (
     <div className ='home'>
       <NavBar/>
+      <div className="hero">
+        <img src={wed_banner} alt='' className='banner-img' />
+        <div className="hero-captiob">
+            <img src={wed_title} alt='' className='caption-img' />
+            <p>A dark, quirky take on the Addams Family’s daughter, as she navigates life at Nevermore Academy while solving mysteries and embracing her psychic powers.</p>
+        </div>
+      </div>
     </div>
   )
 }
