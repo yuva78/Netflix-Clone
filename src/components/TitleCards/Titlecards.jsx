56a4ce44db6083a9315799e7a3935de7710c435e
yuva78 <yu0011an@e.ntu.edu.sg>
2025-11-16 22:55:06 +0800

    added the footer of the homepage

--- src/components/TitleCards/Titlecards.jsx
+++ src/components/TitleCards/Titlecards.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Titlecards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
-const Titlecards = () => {
+const Titlecards = ({title, category}) => {
+  const cardsRef = useRef();
+
+const handleWheel = (event)=>{
+  event.preventDefault();
+  cardsRef.current.scrollLeft += event.deltaY;
+
+}
+
+useEffect(()=>{
+  cardsRef.current.addEventListener('wheel', handleWheel);
+},[])
+  
   return (
     <div className='title-cards'>
-  <h2>Popular on Netflix</h2>
+  <h2>{title?title:"Popular on Netflix"}</h2>
 
-  <div className="card-list">
+  <div className="card-list" ref={cardsRef} >
     {cards_data.map((card, index) => {
       return (
         <div className="card" key={index}>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import wed_title from '../../assets/download.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/Titlecards.jsx'
+import Footer from '../../components/Footer/Footer.jsx'
 
 const Home = () => {
   return (
@@ -23,6 +24,13 @@ const Home = () => {
             <TitleCards/>
         </div>
       </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"}/>
+        <TitleCards title={"Only on Netflix"}/>
+        <TitleCards title={"Upcoming"}/>
+        <TitleCards title={"Top Picks for You"}/>
+      </div>
+      <Footer/>
     </div>
   )
 }
