8bff98606cf1269be5849101a49975a581954b27
yuva78 <yu0011an@e.ntu.edu.sg>
2025-11-17 19:40:48 +0800

    creating the login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
+
 const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
+
+
+
   return (
     <div className ='login'>
-      
+      <img src={logo} className='login-logo' alt="" />
+      <div className= "login-form" >
+        <h1>{signState}</h1>
+        <form >
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          <p>New to Netflix? <span>Sign Up Now</span> </p>
+          <p>Already have an Account? <span>Sign In Now</span> </p>
+        </div>
+      </div>
     </div>
   )
 }
