aecf1a4ea6c1c7ec516d4655b190b8de5178c9d3
yuva78 <yu0011an@e.ntu.edu.sg>
2025-11-17 20:45:21 +0800

    creating the player page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
-      </Routes>
+        <Route path='/player/:id' element={<Player/>}/>
+        </Routes>
     </div>
   );
 }
